3cf7e65417a4b7d2bb8960a174bedb870512a1b8
andropyro28 <menandroeugenio1028@gmail.com>
2022-06-21 03:57:38 -0400

    interfaces

--- typescript-tuts/scripts/js/app.js
+++ typescript-tuts/scripts/js/app.js
@@ -1,5 +1,15 @@
 // classes
 import Invoice from "./classes/Invoice.js"; // we have to use .js because .ts is not recognizeable by browser
+const me = {
+    name: 'andro',
+    age: 21,
+    speak: (name) => `hello ${name}`,
+    spend: (params) => params
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
 const invOne = new Invoice("lyssa", "utang", 1500);
 const invTwo = new Invoice("friends ni doms", "utang", 1500);
 console.log({
--- typescript-tuts/scripts/ts/app.ts
+++ typescript-tuts/scripts/ts/app.ts
@@ -1,6 +1,27 @@
 // classes
 import Invoice from "./classes/Invoice.js"; // we have to use .js because .ts is not recognizeable by browser
 
+
+interface isPerson {
+    name:string,
+    age: number,
+    speak(param: string):void,
+    spend(param: number):number,
+}
+
+const me: isPerson = {
+    name:'andro',
+    age:21,
+    speak: (name:string):string => `hello ${name}`,
+    spend: (params:number):number => params
+}
+
+const greetPerson = (person: isPerson) => {
+    console.log('hello', person.name)
+}
+
+greetPerson(me)
+
 const invOne = new Invoice("lyssa", "utang", 1500);
 const invTwo = new Invoice("friends ni doms", "utang", 1500);
 
@@ -8,6 +29,7 @@ console.log({
   format1: invOne.format,
   format2: invTwo.format,
 });
+
 
 let invoices: Invoice[] = [];
 invoices.push(invOne, invTwo);
